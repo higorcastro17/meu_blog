388e81176f190c9ec5f8088e410d3195b2e417ce
Higor Castro <higorcastro14300302@gmail.com>
2019-10-27 09:50:14 -0300

    Projeto Inicial

---
+++ README.md
@@ -0,0 +1,10 @@
+Projeto: Saúde Odontológica
+Autoresgit add: Higor Pereira de Castro e Amilton Guedes Costa Júnior
+Turma: 2AII
+
+Conteúdo: A odontologia ou medicina dentária é a área da saúde humana que estuda e trata do sistema estomatognático – compreende a face, pescoço e cavidade bucal, abrangendo ossos, musculatura mastigatória, articulações, dentes e tecidos. Tal área é de suma importância para a sociedade, pois, auxilia no tratamento da saúde
+bucal e de doenças relacionadas com a região. 
+
+O projeto possui como finalidade apresentar o fluxo de atendimento odontológico aos universitários graduandos na odontologia, além de informar a importância da saúde bucal para a população global.
+
+Por conseguinte, a página desenvolvida auxiliará e explicará aos discentes e usuários sobre as novidades, especialidades e abordagens atuais na área da odontologia.
---
+++ forms.py
@@ -0,0 +1,46 @@
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField, 
+    PasswordField, 
+    SubmitField, 
+    BooleanField
+)
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class CadastroUsuarioForm(FlaskForm):
+   username = StringField(
+       'Usuário', 
+       validators=[DataRequired(), Length(min=2, max=80)]
+        )
+   email = StringField('Email', validators=[DataRequired(), Email()]) 
+   password = PasswordField('Senha', validators=[DataRequired()])
+   confirm_password = PasswordField('Repita a Senha', validators=[DataRequired(), EqualTo('password')])
+   submit = SubmitField('Cadastrar')
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=80)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Regitrar')
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    
+    remember = BooleanField('Lembre-me')
+    submit = SubmitField('Entrar')
+    """
